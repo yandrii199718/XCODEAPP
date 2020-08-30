216829b86037016c2520e43287622f6517d101a2
yandrii199718 <yandry.tuarez19@gmail.com>
2020-08-30 18:17:11 -0500

    first commit

---
+++ Miapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Miapp
+//
+//  Created by Yandri on 8/30/20.
+//  Copyright © 2020 Yandri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
